1467f22d18bf4b231526ebd273638ddeb7b59d60
bnf <umukorocaleb0@gmail.com>
2025-12-30 20:19:49 +0100

    Updates from abr_store_v2

--- supabase-new-arrivals-migration.sql
+++
@@ -1,6 +0,0 @@
--- Add is_new_arrival field to products table
-ALTER TABLE products ADD COLUMN IF NOT EXISTS is_new_arrival BOOLEAN DEFAULT false;
-
--- Create index for better query performance
-CREATE INDEX IF NOT EXISTS idx_products_new_arrival ON products(is_new_arrival) WHERE is_new_arrival = true;
-
--- types/paystack.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '@paystack/inline-js' {
-  export default class PaystackPop {
-    constructor();
-    resumeTransaction(accessCode: string): void;
-  }
-}
-
